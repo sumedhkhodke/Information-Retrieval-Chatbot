42aa313c155b6afe7681ede7cfcdf1d4351274cd
Sumedh Khodke <sumedh.khodke@gmail.com>
2022-12-04 15:47:10 -0500

    Update createdb.py

--- retrieval/createdb.py
+++ retrieval/createdb.py
@@ -1,5 +1,5 @@
 import mysql.connector
-mydb = mysql.connector.connect(host="34.125.158.59",user="root",password='p4', database="IRProject4Database")
+mydb = mysql.connector.connect(host="localhost",user="root",password='p4', database="IRProject4Database")
 mycursor = mydb.cursor()
 mycursor.execute("CREATE DATABASE IRProject4Database")
 mycursor.execute("CREATE TABLE IRProject4Table (id INT AUTO_INCREMENT PRIMARY KEY, session_id VARCHAR(255), question TEXT(65535), answer TEXT(65535), classifier VARCHAR(255), classifier_probability VARCHAR(255), top_ten_retrieved TEXT(65535), user_feedback VARCHAR(255), total_retrieved VARCHAR(255), DESM_score VARCHAR(255), selected_topic VARCHAR(255), selected_bot_personality VARCHAR(255), meta TEXT(65535))")
