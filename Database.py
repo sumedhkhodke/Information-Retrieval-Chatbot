fafdf75d53457e584d648cd26395c29b9f449ea3
EvaPradhan1 <pradhaneva94@gmail.com>
2022-12-04 12:54:01 -0500

    added meta to db and increased column size
    added meta to db and increased column size

--- Database.py
+++ Database.py
@@ -15,11 +15,20 @@ class Database:
         
         self.mycursor = self.mydb.cursor()
         
+        
+        
+        # self.mydb = mysql.connector.connect(host="localhost",user="pradhaneva94",password='', database="IRProject4Database")
+        # self.mycursor = self.mydb.cursor()
+        # # self.mycursor.execute("CREATE DATABASE IRProject4Database")
+        # self.mycursor.execute("CREATE TABLE IRProject4Table (id INT AUTO_INCREMENT PRIMARY KEY, session_id VARCHAR(255), question TEXT(65535), answer TEXT(65535), classifier VARCHAR(255), classifier_probability VARCHAR(255), top_ten_retrieved TEXT(65535), user_feedback VARCHAR(255), total_retrieved VARCHAR(255), DESM_score VARCHAR(255), selected_topic VARCHAR(255), selected_bot_personality VARCHAR(255), meta TEXT(65535))")
+
+    
+        
         # self.mycursor.execute("CREATE DATABASE IRProject4Database")
         # #1st step without database name in self.mydb
         # self.mycursor.execute("SHOW DATABASES")
         
-        # self.mycursor.execute("CREATE TABLE IRProject4Table (id INT AUTO_INCREMENT PRIMARY KEY, session_id VARCHAR(255), question VARCHAR(255), answer VARCHAR(255), classifier VARCHAR(255), classifier_probability VARCHAR(255), top_ten_retrieved VARCHAR(255), user_feedback VARCHAR(255), total_retrieved VARCHAR(255), DESM_score VARCHAR(255), selected_topic VARCHAR(255), selected_bot_personality VARCHAR(255))")
+        # self.mycursor.execute("CREATE TABLE IRProject4Table (id INT AUTO_INCREMENT PRIMARY KEY, session_id VARCHAR(255), question TEXT(65535), answer TEXT(65535), classifier VARCHAR(255), classifier_probability VARCHAR(255), top_ten_retrieved TEXT(65535), user_feedback VARCHAR(255), total_retrieved VARCHAR(255), DESM_score VARCHAR(255), selected_topic VARCHAR(255), selected_bot_personality VARCHAR(255), meta TEXT(65535))")
         # #2nd step with only first step commented and table name in self.mydb (should be commented after execution)
         # self.mycursor.execute("SHOW TABLES")
 
@@ -105,10 +114,10 @@ class Database:
             
         return x
     
-    def insert_row(self, session_id, question, answer, classifier, classifier_probability, top_ten_retrieved, user_feedback, total_retrieved, DESM_score, selected_topic, selected_bot_personality):
+    def insert_row(self, session_id, question, answer, classifier, classifier_probability, top_ten_retrieved, user_feedback, total_retrieved, DESM_score, selected_topic, selected_bot_personality, meta = "None"):
 
-        sql = "INSERT INTO " + self.table_name + " (session_id, question, answer, classifier, classifier_probability, top_ten_retrieved, user_feedback, total_retrieved,  DESM_score, selected_topic, selected_bot_personality) VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s)"
-        val = [session_id, question, answer, classifier, classifier_probability, top_ten_retrieved, user_feedback, total_retrieved,  DESM_score, selected_topic, selected_bot_personality]
+        sql = "INSERT INTO " + self.table_name + " (session_id, question, answer, classifier, classifier_probability, top_ten_retrieved, user_feedback, total_retrieved,  DESM_score, selected_topic, selected_bot_personality, meta) VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s)"
+        val = [session_id, question, answer, classifier, classifier_probability, top_ten_retrieved, user_feedback, total_retrieved,  DESM_score, selected_topic, selected_bot_personality, meta]
         self.mycursor.execute(sql, val)
 
         self.mydb.commit()
@@ -121,5 +130,5 @@ if __name__ == "__main__":
     
     Database_Project4 = Database()
     
-    # Database_Project4.insert_into()
+    Database_Project4.insert_row("session_id1", "question1", "answer1", "classifier1", "classifier_probability1", "top_ten_retrieved1", "user_feedback1", "total_retrieved1", "DESM_score1", "selected_topic1", "selected_bot_personality1")
     # Database_Project4.retrieve_from()
